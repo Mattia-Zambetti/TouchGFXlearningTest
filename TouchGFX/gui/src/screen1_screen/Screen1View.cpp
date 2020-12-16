0f1013eaa032c06aa114b9cf8176e2c482b3c994
Mattia <mattia.zambetti2000@gmail.com>
2020-12-17 00:00:32 +0100

    ProgressBar multicolor working

--- TouchGFX/gui/src/screen1_screen/Screen1View.cpp
+++ TouchGFX/gui/src/screen1_screen/Screen1View.cpp
@@ -5,6 +5,9 @@
 #include "BitmapDatabase.hpp"
 
 uint32_t ProgressBarValue;
+uint16_t RED;
+uint16_t GREEN;
+uint16_t BLUE=0;
 
 Screen1View::Screen1View()
 {
@@ -39,6 +42,21 @@ void Screen1View::ProgressBarUpdate( uint16_t value )
 	{
     ProgressBarText.setColor(touchgfx::Color::getColorFrom24BitRGB(255, 255, 255));
 	}*/
+	
+	if ( value <= 2048)
+		RED = 255;
+	else
+		RED = 510 - (int)(value*0.12451171875);
+	
+	if ( value <= 2048 )
+		GREEN =	(int) (value * 0.12451171875);
+	else
+		GREEN = 255;
+	
+	
+	lineProgress1Painter.setColor(touchgfx::Color::getColorFrom24BitRGB(RED, GREEN, BLUE));
+  lineProgress1.setPainter(lineProgress1Painter);
+	
 	memset ( &ProgressBarTextBuffer, value, PROGRESSBARTEXT_SIZE);
 	Unicode::snprintfFloat ( ProgressBarTextBuffer, sizeof(ProgressBarTextBuffer), "%.0f %", ProgressBarValue);
 	lineProgress1.setValue((int)ProgressBarValue);
